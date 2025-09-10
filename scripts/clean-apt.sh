4f2873060f853cc6b0ba39f8f4f46513b0389923
xmready <77082140+xmready@users.noreply.github.com>
2025-09-10 16:54:26 -0700

    Update download command from wget to curl

--- scripts/clean-apt.sh
+++ scripts/clean-apt.sh
@@ -7,7 +7,7 @@
 #   A script to autoremove and clean packages using apt
 #
 # Non-root usage:
-#   wget -O- https://raw.githubusercontent.com/xmready/system-setup/main/scripts/clean-apt.sh | bash -
+#   curl -fL https://raw.githubusercontent.com/xmready/system-setup/main/scripts/clean-apt.sh | bash -
 
 echo -e "\n$(tput setaf 3)cleaning up packages\n$(tput sgr0)" \
 && sudo apt-get autoremove -y \
