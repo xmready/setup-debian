eb40c8351601843dcd536e3f97fcd5e10aa3549c
xmready <77082140+xmready@users.noreply.github.com>
2025-09-10 16:37:07 -0700

    Enhance setup-server-shell.sh with documentation
    Added purpose and usage instructions for the script.

--- scripts/setup-server-shell.sh
+++ scripts/setup-server-shell.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/bash
 
-# .bashrc server customization
+# Discussion, issues and change requests at:
+#   https://github.com/xmready/system-setup
+#
+# Purpose:
+#   A script to configure .bashrc for the current user
+#
+# Non-root usage:
+#   curl -fL https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-server-shell.sh | bash -
 
 echo -e "\n$(tput setaf 3)customizing bashrc\n$(tput sgr0)" \
 && echo -e "\n"'export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"' >> ~/.bashrc \
