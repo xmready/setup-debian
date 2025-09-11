bf2f10869e1dcea69746a1892e8bc1d89d25a8bf
xmready <77082140+xmready@users.noreply.github.com>
2025-09-11 15:57:39 -0700

    Fix bashrc customization commands in setup script

--- scripts/setup-server-shell.sh
+++ scripts/setup-server-shell.sh
@@ -10,7 +10,7 @@
 #   curl -fL https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-server-shell.sh | bash -
 
 echo -e "\n$(tput setaf 3)customizing bashrc\n$(tput sgr0)" \
-&& echo -e "\n"'export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"' >> ~/.bashrc \
+&& echo -e "\n"'export PROMPT_COMMAND="history -a; history -n; $PROMPT_COMMAND"' >> ~/.bashrc \
 && echo -e "\n"'stty -ixon' >> ~/.bashrc \
 && sed -i 's/^HISTSIZE.*/HISTSIZE=10000/' ~/.bashrc \
 && sed -i 's/^HISTFILESIZE.*/HISTFILESIZE=20000/' ~/.bashrc \
