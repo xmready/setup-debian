541f891d38150835fdbd8af5efecbc46f0e6c5fe
xmready <77082140+xmready@users.noreply.github.com>
2025-09-11 15:56:52 -0700

    Fix export PROMPT_COMMAND in setup-shell.sh

--- scripts/setup-shell.sh
+++ scripts/setup-shell.sh
@@ -12,7 +12,7 @@
 echo -e "\n$(tput setaf 3)customizing bashrc\n$(tput sgr0)" \
 && echo -e "\n"'stty -ixon' \
   >> ~/.bashrc \
-&& echo -e "\n"'export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"' \
+&& echo -e "\n"'export PROMPT_COMMAND="history -a; history -n; $PROMPT_COMMAND"' \
   >> ~/.bashrc \
 && echo \
   $'\nexport PS1=\'\\n ${debian_chroot:+($debian_chroot)}\[\\033[01;35m\]\\t \[\\033[34m\]\w \[\\033[36m\]$(git branch -v 2>/dev/null)\[\\033[00m\]\\n └─❭ \'' \
