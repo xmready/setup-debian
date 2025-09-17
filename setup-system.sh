10e392bb769b30078e77fa368b603dc88a8ca168
xmready <77082140+xmready@users.noreply.github.com>
2025-09-17 15:58:06 -0700

    Rename setup script variables for URL consistency

--- setup-system.sh
+++ setup-system.sh
@@ -9,39 +9,39 @@
 # sudo usage:
 #   curl -fL https://raw.githubusercontent.com/xmready/system-setup/main/setup-system.sh | bash -
 
-SETUP_APT=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-apt.sh
-SETUP_SHELL=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-shell.sh
-SETUP_TOR=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-tor.sh
-SETUP_SIGNAL=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-signal.sh
-SETUP_NVM=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-nvm.sh
-SETUP_VIM=https://raw.githubusercontent.com/xmready/vim-config/main/setup-vim.sh
-SETUP_RCLONE=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-rclone.sh
-CLEAN_APT=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/clean-apt.sh
-SETUP_FLATPAK=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-flatpak.sh
-SETUP_COMMANDS=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-commands.sh
-HARDEN_NETWORK=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/harden-network.sh
+SETUP_APT_URL=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-apt.sh
+SETUP_SHELL_URL=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-shell.sh
+SETUP_TOR_URL=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-tor.sh
+SETUP_SIGNAL_URL=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-signal.sh
+SETUP_NVM_URL=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-nvm.sh
+SETUP_VIM_URL=https://raw.githubusercontent.com/xmready/vim-config/main/setup-vim.sh
+SETUP_RCLONE_URL=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-rclone.sh
+CLEAN_APT_URL=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/clean-apt.sh
+SETUP_FLATPAK_URL=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-flatpak.sh
+SETUP_COMMANDS_URL=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/setup-commands.sh
+HARDEN_NETWORK_URL=https://raw.githubusercontent.com/xmready/system-setup/main/scripts/harden-network.sh
 
-curl -fL "$SETUP_APT" | bash - \
+curl -fL "$SETUP_APT_URL" | bash - \
 && sleep 3 \
-&& curl -fL "$SETUP_SHELL" | bash - \
+&& curl -fL "$SETUP_SHELL_URL" | bash - \
 && sleep 3 \
-&& curl -fL "$SETUP_TOR" | bash - \
+&& curl -fL "$SETUP_TOR_URL" | bash - \
 && sleep 3 \
-&& curl -fL "$SETUP_SIGNAL" | bash - \
+&& curl -fL "$SETUP_SIGNAL_URL" | bash - \
 && sleep 3 \
-&& curl -fL "$SETUP_NVM" | bash - \
+&& curl -fL "$SETUP_NVM_URL" | bash - \
 && sleep 3 \
-&& curl -fL "$SETUP_VIM" | bash - \
+&& curl -fL "$SETUP_VIM_URL" | bash - \
 && sleep 3 \
-&& curl -fL "$SETUP_RCLONE" | bash - \
+&& curl -fL "$SETUP_RCLONE_URL" | bash - \
 && sleep 3 \
-&& curl -fL "$CLEAN_APT" | bash - \
+&& curl -fL "$CLEAN_APT_URL" | bash - \
 && sleep 3 \
-&& curl -fL "$SETUP_FLATPAK" | bash - \
+&& curl -fL "$SETUP_FLATPAK_URL" | bash - \
 && sleep 3 \
-&& curl -fL "$SETUP_COMMANDS" | bash - \
+&& curl -fL "$SETUP_COMMANDS_URL" | bash - \
 && sleep 3 \
-&& curl -fL "$HARDEN_NETWORK" | bash - \
+&& curl -fL "$HARDEN_NETWORK_URL" | bash - \
 && sleep 3 \
 && echo -e \
   "\n$(tput setaf 1)$(tput bold)SYSTEM WILL REBOOT IN 60 SECONDS\n$(tput sgr0)$(tput bel)" \
